cc91601f746868d91388522eb5cdaf84d072b076
Florin Grama <programelu@gmail.com>
2020-11-22 12:41:39 +0200

    tuples

---
+++ fundamentals/tuples/simple-tuples.py
@@ -0,0 +1,21 @@
+"""tuples may have paranthesys or not"""
+
+#tuple with paranthesys, recommanded for redability reasons
+plane1 = ("Airbus", "319", 200)
+print(plane1)
+
+
+#tuple without paranthesys - recognized as tuples because comma has been encountered
+plane2 = "Airbus", "A380", 500
+print(plane2)
+
+
+#tuples are immutable, cannot change anything from a tuple but create and assign another value
+plane2 = "Boeing" ,"777", 380
+print(plane2)
+
+print(plane2[0])
+
+#cannot assign a value to an item of the tuple - immutability
+plane2[0] = "Airbus"
+
